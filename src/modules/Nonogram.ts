4e6b3f9a75e607922bf0eece100cfe5b07d63f59
Timon Kobusch <timon_kobusch@web.de>
2024-03-10 18:23:10 +0100

    Refactor Nonogram Class

--- src/modules/Nonogram.ts
+++ src/modules/Nonogram.ts
@@ -5,114 +5,102 @@ export enum Marking {
     CROSSING = 2,
     EMPTY = 0,
 }
-
-function createRandomGrid(size: number) {
-    const grid = Array.from({ length: size }, () => Array.from({ length: size }, () => 0));
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            Math.random() > 0.3 ? (grid[i][j] = Marking.MARKING) : (grid[i][j] = Marking.EMPTY);
-        }
-    }
-
-    return grid;
-}
-
-function createHints(grid: number[][]) {
-    const rows = grid.map((row) => {
-        const hints = [];
-        let count = 0;
-        for (let i = 0; i < row.length; i++) {
-            if (row[i] === Marking.MARKING) {
-                count++;
-            } else if (count > 0) {
-                hints.push(count);
-                count = 0;
-            }
-        }
-        if (count > 0) {
-            hints.push(count);
-        }
-        if (hints.length === 0) {
-            hints.push(0);
-        }
-        return hints;
-    });
-    const columns = Array.from({ length: grid.length }, () => []);
-    for (let i = 0; i < grid.length; i++) {
-        let count = 0;
-        for (let j = 0; j < grid.length; j++) {
-            if (grid[j][i] === Marking.MARKING) {
-                count++;
-            } else if (count > 0) {
-                (columns[i] as number[]).push(count);
-                count = 0;
-            }
-        }
-        if (count > 0) {
-            (columns[i] as number[]).push(count);
-        }
-        if (columns[i].length === 0) {
-            (columns[i] as number[]).push(0);
-        }
-    }
-    return { rows, columns };
-}
-
 export class Nonogram {
-    size: number;
-    grid: number[][];
-    solution: number[][];
-    progress: {
+    size!: number;
+    grid!: number[][];
+    solution!: number[][];
+    progress!: {
         cellsToBeMarked: number;
         cellsMarked: number;
         isWon: boolean;
     };
-    hints: {
+    hints!: {
         rows: number[][];
         columns: number[][];
     };
-    finishedLines: {
+    finishedLines!: {
         rows: boolean[];
         columns: boolean[];
     };
 
     constructor(arg: number | Nonogram) {
         if (typeof arg === 'number') {
-            this.size = arg as number;
-            this.grid = Array.from({ length: this.size }, () => Array.from({ length: this.size }, () => Marking.EMPTY));
-            this.solution = createRandomGrid(this.size);
-            this.hints = createHints(this.solution);
-            let solver = new NonogramHumanSolver(this.size, this.hints, this.solution);
-            while (!solver.solve()) {
-                this.solution = createRandomGrid(this.size);
-                this.hints = createHints(this.solution);
-                solver = new NonogramHumanSolver(this.size, this.hints, this.solution);
-            }
-            this.progress = {
-                cellsToBeMarked: this.solution.flat().filter((cell) => cell === Marking.MARKING).length,
-                cellsMarked: 0,
-                isWon: false,
-            };
-            this.finishedLines = {
-                rows: Array.from({ length: this.size }, () => false),
-                columns: Array.from({ length: this.size }, () => false),
-            };
-
-            return;
+            this.initializeNewNonogram(arg);
+        } else {
+            this.copyNonogram(arg);
         }
-        const nonogram = arg as Nonogram;
-        this.size = nonogram.size;
-        this.grid = nonogram.grid.map((row) => row.slice());
-        this.solution = nonogram.solution.map((row) => row.slice());
-        this.progress = {
-            cellsToBeMarked: nonogram.progress.cellsToBeMarked,
-            cellsMarked: nonogram.progress.cellsMarked,
-            isWon: nonogram.progress.isWon,
-        };
-        this.hints = nonogram.hints;
-        this.finishedLines = nonogram.finishedLines;
     }
-    reset() {
+
+    private static createRandomGrid(size: number) {
+        const grid = Array.from({ length: size }, () => Array.from({ length: size }, () => 0));
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                Math.random() > 0.3 ? (grid[i][j] = Marking.MARKING) : (grid[i][j] = Marking.EMPTY);
+            }
+        }
+
+        return grid;
+    }
+
+    private static createHints(grid: number[][]): { rows: number[][]; columns: number[][] } {
+        const rows = grid.map((row) => Nonogram.extractHintsFromLine(row));
+        const columns = Array.from({ length: grid.length }, (_, i) => Nonogram.extractHintsFromLine(grid.map((row) => row[i])));
+        return { rows, columns };
+    }
+
+    private static extractHintsFromLine(line: number[]): number[] {
+        const hints = line.reduce(
+            (acc, mark, i, src) => {
+                if (mark === Marking.MARKING) {
+                    acc[acc.length - 1] = (acc[acc.length - 1] || 0) + 1;
+                } else if (i > 0 && src[i - 1] === Marking.MARKING) {
+                    acc.push(0);
+                }
+                return acc;
+            },
+            [0]
+        );
+        return hints.length > 1 || hints[0] !== 0 ? hints.filter((hint) => hint !== 0) : [0];
+    }
+
+    private initializeNewNonogram(size: number): void {
+        this.size = size;
+        this.grid = Nonogram.createEmptyGrid(this.size);
+        this.solution = Nonogram.createRandomGrid(this.size);
+        this.hints = Nonogram.createHints(this.solution);
+
+        let solver = new NonogramHumanSolver(this.size, this.hints, this.solution);
+        while (!solver.solve()) {
+            this.solution = Nonogram.createRandomGrid(this.size);
+            this.hints = Nonogram.createHints(this.solution);
+            solver = new NonogramHumanSolver(this.size, this.hints, this.solution);
+        }
+
+        this.progress = {
+            cellsToBeMarked: this.solution.flat().filter((cell) => cell === Marking.MARKING).length,
+            cellsMarked: 0,
+            isWon: false,
+        };
+        this.finishedLines = {
+            rows: Array.from({ length: this.size }, () => false),
+            columns: Array.from({ length: this.size }, () => false),
+        };
+    }
+
+    private copyNonogram(nonogram: Nonogram): void {
+        this.size = nonogram.size;
+        this.grid = nonogram.grid.map((row) => [...row]);
+        this.solution = nonogram.solution.map((row) => [...row]);
+        this.progress = { ...nonogram.progress };
+        this.hints = nonogram.hints;
+        this.finishedLines = { ...nonogram.finishedLines };
+    }
+
+    private static createEmptyGrid(size: number): Marking[][] {
+        return Array.from({ length: size }, () => Array.from({ length: size }, () => Marking.EMPTY));
+    }
+
+    public reset(): void {
         this.grid = Array.from({ length: this.size }, () => Array.from({ length: this.size }, () => Marking.EMPTY));
         this.progress = {
             cellsToBeMarked: this.progress.cellsToBeMarked,
@@ -120,43 +108,8 @@ export class Nonogram {
             isWon: false,
         };
     }
-    click(x: number, y: number, marking: boolean) {
-        if (this.grid[x][y] === Marking.EMPTY && marking) {
-            this.progress.cellsMarked++;
-            this.grid[x][y] = Marking.MARKING;
-        } else if (this.grid[x][y] === Marking.EMPTY && !marking) {
-            this.grid[x][y] = Marking.CROSSING;
-        } else if (this.grid[x][y] === Marking.MARKING && marking) {
-            this.progress.cellsMarked--;
-            this.grid[x][y] = Marking.EMPTY;
-        } else if (this.grid[x][y] === Marking.CROSSING && !marking) {
-            this.grid[x][y] = Marking.EMPTY;
-        }
-        this.checkFinishedLines();
-        this.checkWin();
-    }
 
-    setCell(x: number, y: number, marking: boolean, clearMode: boolean) {
-        if (clearMode) {
-            if (marking && this.grid[x][y] === Marking.MARKING) {
-                this.progress.cellsMarked--;
-                this.grid[x][y] = Marking.EMPTY;
-            } else if (!marking && this.grid[x][y] === Marking.CROSSING) {
-                this.grid[x][y] = Marking.EMPTY;
-            }
-            this.checkWin();
-            return;
-        }
-        if (this.grid[x][y] === Marking.EMPTY && marking) {
-            this.progress.cellsMarked++;
-            this.grid[x][y] = Marking.MARKING;
-        } else if (this.grid[x][y] === Marking.EMPTY && !marking) {
-            this.grid[x][y] = Marking.CROSSING;
-        }
-        this.checkFinishedLines();
-        this.checkWin();
-    }
-    checkWin() {
+    private checkWin(): void {
         for (let i = 0; i < this.size; i++) {
             for (let j = 0; j < this.size; j++) {
                 if (this.grid[i][j] !== this.solution[i][j]) {
@@ -177,48 +130,63 @@ export class Nonogram {
 
         this.progress.isWon = true;
     }
-    checkFinishedLines() {
-        function checkEqualArray(array1: number[], array2: number[]) {
-            if (array1.length !== array2.length) {
-                return false;
-            }
 
-            for (let i = 0; i < array1.length; i++) {
-                if (array1[i] !== array2[i]) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        function countGroups(arr: number[]) {
-            const result = [];
-            let count = 0;
-            for (let i = 0; i < arr.length; i++) {
-                if (arr[i] === 1) {
-                    count++;
-                }
-                if (arr[i] !== 1 || i === arr.length - 1) {
-                    if (count > 0) {
-                        result.push(count);
-                        count = 0;
-                    }
-                }
-            }
-            return result.filter((group) => group !== 0);
-        }
-
-        for (let rowIndex = 0; rowIndex < this.size; rowIndex++) {
-            const result = checkEqualArray(countGroups(this.grid[rowIndex]), this.hints.rows[rowIndex]);
-            this.finishedLines.rows[rowIndex] = result;
-        }
-        for (let columnIndex = 0; columnIndex < this.size; columnIndex++) {
-            const result = checkEqualArray(countGroups(this.grid.map((row) => row[columnIndex])), this.hints.columns[columnIndex]);
-            this.finishedLines.columns[columnIndex] = result;
-        }
+    private checkFinishedLines(): void {
+        this.finishedLines.rows = this.grid.map((row, rowIndex) =>
+            Nonogram.areHintsMatching(Nonogram.extractHintsFromLine(row), this.hints.rows[rowIndex])
+        );
+        this.finishedLines.columns = this.grid[0].map((_, columnIndex) => {
+            const column = this.grid.map((row) => row[columnIndex]);
+            return Nonogram.areHintsMatching(Nonogram.extractHintsFromLine(column), this.hints.columns[columnIndex]);
+        });
     }
-    forceWin() {
+
+    private static areHintsMatching(lineHints: number[], hintArray: number[]): boolean {
+        if (lineHints.length !== hintArray.length) return false;
+        for (let i = 0; i < lineHints.length; i++) {
+            if (lineHints[i] !== hintArray[i]) return false;
+        }
+        return true;
+    }
+
+    public click(x: number, y: number, marking: boolean): void {
+        if (this.grid[x][y] === Marking.EMPTY && marking) {
+            this.progress.cellsMarked++;
+            this.grid[x][y] = Marking.MARKING;
+        } else if (this.grid[x][y] === Marking.EMPTY && !marking) {
+            this.grid[x][y] = Marking.CROSSING;
+        } else if (this.grid[x][y] === Marking.MARKING && marking) {
+            this.progress.cellsMarked--;
+            this.grid[x][y] = Marking.EMPTY;
+        } else if (this.grid[x][y] === Marking.CROSSING && !marking) {
+            this.grid[x][y] = Marking.EMPTY;
+        }
+        this.checkFinishedLines();
+        this.checkWin();
+    }
+
+    public setCell(x: number, y: number, marking: boolean, clearMode: boolean): void {
+        if (clearMode) {
+            if (marking && this.grid[x][y] === Marking.MARKING) {
+                this.progress.cellsMarked--;
+                this.grid[x][y] = Marking.EMPTY;
+            } else if (!marking && this.grid[x][y] === Marking.CROSSING) {
+                this.grid[x][y] = Marking.EMPTY;
+            }
+            this.checkWin();
+            return;
+        }
+        if (this.grid[x][y] === Marking.EMPTY && marking) {
+            this.progress.cellsMarked++;
+            this.grid[x][y] = Marking.MARKING;
+        } else if (this.grid[x][y] === Marking.EMPTY && !marking) {
+            this.grid[x][y] = Marking.CROSSING;
+        }
+        this.checkFinishedLines();
+        this.checkWin();
+    }
+
+    public forceWin(): void {
         this.progress.isWon = true;
     }
 }
